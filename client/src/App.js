b8a381857db4852e975ad9395efc4912393a5338
RLacer <r.a.lacer@hotmail.com>
2021-07-29 12:06:48 -0600

    latest updated

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import "./App.css";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
+// import PetForm from "./components/PetForm";
 
 
 // Construct our main GraphQL API endpoint
@@ -51,10 +52,7 @@ function App () {
         <Route exact path="/Login">
          <Login />
          </Route>
-<<<<<<< HEAD
          {/* <PetForm /> */}
-=======
->>>>>>> 57402ee269189d9f7ceb85afcc24bb2628d6aaff
          </Switch>
         </Router>
       </ApolloProvider>
--- client/src/components/PetForm/index.js
+++ client/src/components/PetForm/index.js
@@ -1,4 +1,5 @@
 // import React,  { useState } from "react";
+// import { Link } from 'react-router-dom';
 // import { useMutation } from '@apollo/client';
 // import { ADD_PET } from "../../utils/mutations";
 // import { QUERY_PETS } from "../../utils/queries";
@@ -10,58 +11,76 @@
 // // import { ADD_PET } from '../utils/mutations';
 // // import { QUERY_PETS } from '../../utils/queries';
 // const PetForm = () => {
-//   const [dogName,  setdogName] = useState ('');
+//   const [pet,  setPet] = useState ('');
+//   const [addPet, { error }] = useMutation(ADD_PET, {
+//     update(cache, { data: { addPet }}) {
+//       try {
+//         const { pets } = cache.readQuery({ query: QUERY_PETS });
+//         Cache.writeQuery({
+//           query: QUERY_PETS,
+//           data: {pets: [addPet,  ...pets]},
+//         });
   
-//   const handleForm = async (e) => {
+//       } catch (e) {
+//         console.error(e);
+//       }
+//     }
+//   })
+  
+  
+//   const handleFormSubmit = async (e) => {
 //     e.preventDefault();
 //     console.log("hello")
-//     const [addPet, { error }] = useMutation(ADD_PET, {
-//       update(cache, { data: { addPet }}) {
-//         try {
-//           const { pets } = cache.readQuery({ query: QUERY_PETS });
-//           Cache.writeQuery({
-//             query: QUERY_PETS,
-//             data: {pets: [addPet,  ...pets]},
-//           });
-    
-//         } catch (e) {
-//           console.error(e);
-//         }
-//       }
-//     })
+   
     
 //   try {
 //     const { data } = await addPet({
-//       variables: {
-//         dogName,
-//         // dogBreed,
-//         // dogGender,
-//         // dogAge
-//       }
-//     })
-//     setdogName('')
-//   } catch (err) {
-//     console.error(err);
-//   }
-// } 
+//     //   addPet: {
+//     //       variables:
+//     //     dogName,
+//     //     dogBreed,
+//     //     dogGender,
+//     //     dogAge
+//     //   }
+//     // })
+// //     setPet('')
+// //   } catch (err) {
+// //     console.error(err);
+// //   }
+// } ,
 
-// function handleChange(e) {
-//   const { value } = e.target;
-//   setdogName(value)
-//   console.log(dogName)
-// }
+
+// const handleChange = (event) => {
+//     const { pet, value } = event.target;
+
+//     if (pet === 'addPet' && value.length <= 280) {
+//       setPet(value);
+     
+//     }
+//   };
 
 //   return (
 //     <form onSubmit={handleInput}>
-//       <input className="dogName" id="dogName" placeholder="dog name" value={dogName} onChange={handleChange}></input>
-//       <input className="dogBreed" id="dogBreed" placeholder="dog breed"></input>
+//       <input className="dogName" id="dogName" placeholder="dog name" onSubmit={handleFormSubmit}></input>
+//       <input className="dogBreed" id="dogBreed" placeholder="dog breed" onSubmit={handleFormSubmit}></input>
 //       <input
 //         className="dogGender"
 //         id="dogGender"
 //         placeholder="dog gender"
 //       ></input>
-//       <input className="dogAge" id="dogAge" placeholder="dog age"></input>
+//          <div className="col-12 col-lg-9">
+//               <textarea
+//                 name="pet"
+               
+//                 value={addPet}
+//                 className="form-input w-100"
+//                 style={{ lineHeight: '1.5', resize: 'vertical' }}
+//                 onChange={handleChange}
+//               ></textarea>
+//             </div>
+//       <input className="dogAge" id="dogAge" placeholder="dog age" onSubmit={handleFormSubmit}></input>
 //       <button className="submit" id="submit" type="submit">Add Pet</button>
+
 //     </form>
 //   );
 // };
