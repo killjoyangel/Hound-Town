282e0c22e63df9a6b56a77ffd049b0f95fd5084d
Ariel Marchand <marchand.aa@gmail.com>
2021-07-28 18:14:26 -0600

    pull

--- server/models/pets.js
+++ server/models/pets.js
@@ -1,26 +1,41 @@
 const { Schema, model } = require('mongoose');
 
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const petSchema = new Schema({
- dogBreed: {
+  dogBreed: {
     type: String,
+    minlength:1,
+    maxlenght:20,
     required: 'You need to enter a breed!',
     trim: true,
   },
   dogName: {
     type: String,
+    minlength:1,
+    maxlenght:20,
     required: 'You need to enter a name!',
     trim: true,
   },
-  dogGender: {
+  dogGender:{
+  type: String,
+  minlength:1,
+  maxlenght:7,
+  required: 'You need to enter a gender!',
+  trim: true,
+  },
+  dogAge:{
     type: String,
+    minlength:1,
+    maxlenght:2,
+    required: 'You need to enter an age!',
     trim: true,
-  },
-  dogAge: {
-    type: Integer,
-    trim: true,
-  },
+    },
+    User: {
+      type:Schema.Types.ObjectId,
+      ref: 'User'
+    }
+
 });
 
-const Pets = model('Pets', petSchema);
-
-module.exports = pets;
+const Pet = model('Pets', petSchema);
+module.exports = Pet;
--- server/models/user.js
+++ server/models/user.js
@@ -1,32 +1,55 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Thought',
-    },
-  ],
-});
+// import schema from Pets.js
+//const petSchema = require('./Pets');
+//const { STRING } = require('sequelize/types');
 
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 1,
+    },
+    Pets: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Pet'
+      }
+    ],
+
+  });
+
+  const User = model('User', userSchema);
+
+  module.exports = User;
+  
+
+
+
+
+
+    // pets: [
+    //   {
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'Pets',
+    //   },
+    
+
+/* hash user password
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
@@ -34,13 +57,18 @@ userSchema.pre('save', async function (next) {
   }
 
   next();
-});
+});*/
 
+/* custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
+ when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
+userSchema.virtual('petCount').get(function () {
+  return this.savedPetslength;
+});
+
 const User = model('User', userSchema);
 
-module.exports = User;
- 
+module.exports = User;*/
